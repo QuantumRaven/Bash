8bacb1cdc8e160e5369b2c7ae155b04ddf0cd313
QuantumRaven <quantum-raven@outlook.com>
2020-12-16 22:42:37 -0500

    Updated file

---
+++ Debian/devtools_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt install -y build-essential \
+zlib1g-dev \
+libncurses5-dev \
+libgdbm-dev \
+libnss3-dev \
+libssl-dev \
+libsqlite3-dev \
+libreadline-dev \
+libffi-dev \
+curl \
+libbz2-dev
