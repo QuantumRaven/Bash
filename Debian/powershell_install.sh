cc05a9504ea6a9bc08bda848f49776bee3758f79
QuantumRaven <quantum-raven@outlook.com>
2020-12-17 16:11:04 -0500

    Updated comment

--- Debian/powershell_install.sh
+++ Debian/powershell_install.sh
@@ -3,7 +3,7 @@
 ######################################################################### 
 # Original Author: Chloe Carpenter
 #
-# Role of script: Install PowerShell 7.0
+# Role of script: Install PowerShell 7.x
 #########################################################################
 wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
@@ -11,4 +11,4 @@ dpkg -i packages-microsoft-prod.deb
 apt update -y
 apt install -y powershell
 
-printf "To start PowerShell, type pwsh and press enter \n"
+echo "To start PowerShell, type pwsh and press enter \n"
